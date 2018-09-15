86e49091e5116b97bab036c1d685d775272d7f47
realnata <41498923+realnata@users.noreply.github.com>
2018-09-15 23:03:12 +0300

    assignment have done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In the assignment I create a functions that count inverse matrix and 
+## values of matrix and inverse matrix write in Cache.
 
-## Write a short comment describing this function
+##The  function, makeCacheMatrix creates a matrix, which is really 
+##a list containing a function to set and get the value of the matrix and
+##set and get the value of the inverse of matrix (solve)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## In the function calculates 
+## the solve of the matrix created with the above function. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
